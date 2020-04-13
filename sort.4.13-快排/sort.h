53c252076374189eb5fac1408da45c6e46a173f8
jiewaikexue <2947471438@qq.com>
2020-04-13 21:45:49 +0800

    快排

---
+++ sort.4.13-快排/sort.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void swap(int* array, int i, int j);
+void insertSort(int* array, int n);
+void shellSort(int* array, int n);
+void selectSort(int* array, int n);
+void selectSort2(int* array, int n);
+
+void shiftDown(int* array, int n, int parent);
+
+void heapSort(int* array, int n);
+
+void bubbleSort(int* array, int n);
+
+int partion(int* array, int begin, int end);
+
+int partion2(int* array, int begin, int end);
+
+int partion3(int* array, int begin, int end);
+
+
+void quickSort(int* array, int begin, int end);
+
+
+
